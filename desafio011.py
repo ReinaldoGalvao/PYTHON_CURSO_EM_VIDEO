e76a4dd3a54ed5ec80a518481e545c8a1132ecb6
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-06-30 14:05:26 -0300

    Create desafio011.py

---
+++ desafio011.py
@@ -0,0 +1,9 @@
+largura = float(input('Digite a largura: '))
+altura = float(input("Digite a altura: "))
+
+area = largura * altura
+qts_tinta = area / 2
+
+print(f"A área da parede é de {area:.2f} metros quadrados.")
+print(f"Serão necessários {qts_tinta:.2f} litros de tinta para pintar a parede.")
+
