8239d515f3e1cda29ebee9e86db0d27697d590c8
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-03 15:37:21 -0300

    Create desafio26.py

---
+++ desafio26.py
@@ -0,0 +1,25 @@
+"""
+Faça um programa que leia uma frase pelo teclado e mostre:
+
+01 Quantas vezes aparece a letra "A" 
+02 Em que posiçao aparece primeira vez a letra "A"
+03 Em que posiçao aparece a letra "A" utima vez   
+
+"""
+
+frase = input('Digite um texto: ')
+
+# Quantidade de vezes que a letra "A" aparece
+frase_a = frase.upper().count('A')
+print(f"A letra 'A' aparece {frase_a} vezes na frase.")
+
+# Posição da primeira ocorrência da letra "A"
+frase_primeiro_a = frase.upper().find('A')
+print(f"A primeira ocorrência da letra 'A' está na posição {frase_primeiro_a}.")
+
+# Posição da última ocorrência da letra "A"
+frase_ultimo_a = frase.upper().rfind('A')
+print(f"A última ocorrência da letra 'A' está na posição {frase_ultimo_a}.")
+
+qtsletrasa = frase.upper().count('A')
+print(f'A letra "A" apareceu {qtsletrasa} na frase.')
