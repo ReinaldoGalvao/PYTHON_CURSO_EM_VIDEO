edaf24e0ba51910d93b3a296497cd110d900fce2
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-05 08:54:29 -0300

    desafio

--- desafio030.py
+++ desafio030.py
@@ -4,7 +4,13 @@ Crie um programa que leia um numero inteiro e mostre na tela se é PAR ou IMPAR.
 
 n = int(input('Diga um numero: '))
 
-if (n + n)/2 % 2:
+"""if (n + n)/2 % 2:
      print('Impar')
 else:
-    print('Par')
+    print('Par')"""
+    
+resultado = n % 2
+if resultado == 0:
+    print(f'O numero {n} é par.')
+else:
+    print(f'O numero {n} é impar.')
--- desafio035.py
+++ desafio035.py
@@ -2,6 +2,12 @@
 Crie um programa que  leia o comprimento de 3 retas
 e diga se ao usuario se elas poder ou nao formar um triangulo
 '''
+cores = {
+    'limpa':'\033[m',
+    'azul':'\033[34m',
+    'amarelo':'\033[33m',
+    'pretoebranco':'\033[7:30m'
+    }
 
 r1 = int(input('Diaga o comprimento da primeira reta: '))
 r2 = int(input('Diaga o comprimento da segunda reta: '))
