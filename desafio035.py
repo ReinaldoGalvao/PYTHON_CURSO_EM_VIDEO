c3b84d0a2fbf41d08b3ba7d0325a1b93e6463319
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-05 11:50:17 -0300

    Update desafio035.py

--- desafio035.py
+++ desafio035.py
@@ -2,6 +2,8 @@
 Crie um programa que  leia o comprimento de 3 retas
 e diga se ao usuario se elas poder ou nao formar um triangulo
 '''
+from rich import print
+
 cores = {
     'limpa':'\033[m',
     'azul':'\033[34m',
@@ -16,4 +18,4 @@ r3 = int(input('Diaga o comprimento da terceira reta: '))
 if (r2 - r3) < r1 < r2 + r3 or (r1 - r3) < r2 < r1 + r3 or (r1 - r2) < r3 < r1 + r2:
     print('Pode formar um triangulo')
 else:
-    print('Nao pode')
+    print('\033[0;34;4Nao pode')
