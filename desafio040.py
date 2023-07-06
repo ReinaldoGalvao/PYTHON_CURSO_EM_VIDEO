84303d38f85f1f7a0b91b02d4587442e5395d100
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-06 10:27:02 -0300

    Create desafio040.py

---
+++ desafio040.py
@@ -0,0 +1,18 @@
+#crie um programa que leia 2 notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+
+notas = []
+
+for i in range(2):
+    nota = float(input('Diga a nota: '))
+    notas.append(nota)
+    
+media = (notas[0] + notas[1]) /2
+
+if media < 5.0:
+    print(f'Sua media foi {media} e você está REPROVADO.')
+    
+elif media > 5.0 and media < 6.9 :
+    print(f'Sua media foi {media} e você está RECUPERAÇÃO.')
+    
+elif media >= 7.0:
+    print(f'Sua media foi {media} e você está APROVADO.') 
