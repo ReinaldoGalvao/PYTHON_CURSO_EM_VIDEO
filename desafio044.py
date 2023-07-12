aed1130a4f74231cca6db0999a4e5cf119e6950c
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-12 18:48:31 -0300

    Update desafio044.py

--- desafio044.py
+++ desafio044.py
@@ -6,16 +6,47 @@ preço normal e condiçao de pagamento
 2x no cartao: 5% de desconto
 3x ou mais no cartao: 20% juros
 '''
+nome_loja = 'LOJAS BEMOS'
+print(f'{nome_loja:=^40}')
 
+preco = float(input('Diga o valor da compra: '))
 
-preco_normal = float(input('Dia o valor da caisa: '))
+print('''FORMAS DE PAGAMENTO 
+[1] à vista DINHEIRO/PIX
+[2] à vista CARTÃO
+[3] 2X no CARTÃO
+[4] 3X OU mais no CARTÃO''')
 
-x2_cartao = preco_normal - (preco_normal * 0.05)
+opcao = input('Digite qual é a opção: ')
 
-x3_cartao = ( preco_normal * 0.20 ) + preco_normal
+while not opcao.isdigit() or int(opcao) not in [1, 2, 3, 4]:
+    print('Opção inválida. Tente novamente.')
+    opcao = input('Digite qual é a opção: ')
 
-print(preco_normal)
-print(x2_cartao)
-print(x3_cartao)
+opcao = int(opcao)  # Converter para número inteiro
 
-print(f'O preço da camisa custa: \n Preço normal {preco_normal - 0.005:.2f} \n Preço 2x cartão {x2_cartao - 0.005} \n Preço 3x cartão ou mais {x3_cartao - 0.005:.2f}   ')
+if opcao == 1:
+    total = preco - (preco * 0.10)
+    # Restante do código...
+elif opcao == 2:
+    total = preco - (preco * 0.05)
+    # Restante do código...
+elif opcao == 3:
+    total = preco
+    parcela = total / 2
+    print(f'Sua compra será parcelada em 2x de R${parcela:.2f}')
+elif opcao == 4:
+    total = preco + (preco * 0.20)
+    totparc = input('Digite quantas parcelas: ')
+    
+    while not totparc.isdigit():
+        totparc = input('Valor inválido. Digite novamente a quantidade de parcelas: ')
+    
+    totparc = int(totparc)
+    parcela = total / totparc
+    print(f'Sua compra será parcelada em {totparc}x de R${parcela:.2f} com juros')
+    
+else:
+    print('Opção inválida. Tente novamente.')
+
+print(f'Sua compra de R${preco:.2f} vai custar R${total:.2f} no final.')
