01c964205923fd9a71bc68575426c86a831aa912
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-14 16:38:55 -0300

    Create desafio046.py

---
+++ desafio046.py
@@ -0,0 +1,8 @@
+'''faça um programa que mostre na tela uma contagem regressiva
+para o estouro de fogos de artificios, indo de 10 até 0, com uma pausa de 1 segundo antre eles.
+'''
+import time
+for c in range(10, -1, -1):
+    print(c)
+    time.sleep(1)
+    
