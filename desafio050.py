fa997bea89a51a1193c712d4fde9e4358e65b000
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-21 16:35:55 -0300

    Update desafio050.py

--- desafio050.py
+++ desafio050.py
@@ -4,11 +4,13 @@ Se o valor digitado for impar, desconsidere-o.
 '''
 
 soma = 0
+qts = 0
 
 for _ in range(6):
     numero = int(input("Digite um número inteiro: "))
     
     if numero % 2 == 0:
         soma += numero
+        qts += 1
 
-print("A soma dos números pares é:", soma)
+print("A soma dos {} números pares digitados é: {}".format(qts, soma))
