80f27f6c8203672678f9460b7fd99341db5e0c60
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-21 17:15:29 -0300

    Update desafio051.py

--- desafio051.py
+++ desafio051.py
@@ -2,10 +2,15 @@
 Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
 No final, mostre os 10 primeiros termos dessa pregressão
 '''
-
-numero = int(input('Diga um número: '))
+print('=================================')
+print('=      10 TERMOS DE UMA PA      =')
+print('=================================')
+primeiro = int(input('Diga o primeiro termo: '))
 razao = int(input('Diga a razão: '))
-x = int(input('Diga qunatas vezes: '))
-
-for c in range(numero, x, razao):
-    print (c)
+decimo_termo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo_termo + razao, razao):
+    if c % 2 == 0:
+        print('{}'.format(c), end='-> ')
+    elif c % 2 != 0:
+        print('{}'.format(c), end='-> ')
+print('ACABOU')
