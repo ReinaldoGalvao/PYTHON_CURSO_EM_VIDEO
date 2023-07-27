d9f1d9655d848c36cabf7e50d6711af5f6a53c96
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-27 08:15:57 -0300

    Update desafio056.py

--- desafio056.py
+++ desafio056.py
@@ -13,6 +13,10 @@ sexo = ''
 total_masculino = 0
 total_feminino = 0
 soma_idade = 0
+nome_Mvelho = ''
+nome_Mvelhoh = ''
+idadeveriveio = 0
+idadeveriveioh = 0
 def contem_apenas_letras(texto):
     return texto.isalpha()
 
@@ -24,26 +28,35 @@ for i in range(1, 5):
     while not contem_apenas_letras(nome):
         print('Nome inválido. Digite apenas letras, sem números ou caracteres especiais.')
         nome = input('Diga o nome da {0}° pessoa: '.format(i))
-    
+
     idade_str = input('Diga a idadade da {0}° pessoa: '.format(i))
     while not contem_apenas_numeros(idade_str):
         print('idade inválida. Digite apenas numeros, sem letras ou caracteres especiais.')
         idade_str = input('Diga a idade da {0}° pessoa: '.format(i))
         
     idade = int(idade_str)
-    soma_idade += idade
     
     sexo = input('Diga o sexo da {0}° pessoa: '.format(i)).strip().upper()
     while sexo not in ('M', 'F'):
         print('Sexo invalido!')
         sexo = input('Diga o sexo da {0}° pessoa: '.format(i)).strip().upper()
-    idade = int(idade)
+    
     soma_idade += idade
-    if sexo == 'M':
-        total_masculino += 1
-    else:
+    if sexo == 'F':
         total_feminino += 1
+        if idadeveriveio < idade:
+            idadeveriveio = idade
+            nome_Mvelho = nome
+    else:
+        total_masculino += 1
+        if idadeveriveioh < idade:
+            idadeveriveioh = idade
+            nome_Mvelhoh = nome
+
 media_idade = soma_idade / 4
+
 print(f'Total de pessoas do sexo masculino: {total_masculino}')
 print(f'Total de pessoas do sexo feminino: {total_feminino}')
 print(f'Média de idades: {media_idade:.2f}')
+print(f'A mulher mais velha do grupo : {nome_Mvelho} com a idade de {idadeveriveio} anos.')
+print(f'A mulher mais velha do grupo : {nome_Mvelhoh} com a idade de {idadeveriveioh} anos.')
