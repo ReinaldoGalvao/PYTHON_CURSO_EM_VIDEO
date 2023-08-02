818a5204843d195605b334c48945b4648d2c2a0c
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-08-02 14:09:35 -0300

    Update desafio065.py

--- desafio065.py
+++ desafio065.py
@@ -4,25 +4,32 @@ qts = 0
 soma = 0
 resposta = ''
 media = 0
+maior= 0
+menor = 0
+primeira_vez = True
 
 while True:
     if resposta == 'S':
         break
     while True:
-        if resposta == 'S':
+        numero = int(input('Diga um numero: '))
+        qts += 1
+        soma = soma + numero
+        if primeira_vez:
+            menor = numero + 1
+            primeira_vez = False
+        if numero > maior:
+            maior = numero
+        if menor > numero:
+            menor = numero
+        resposta = input('Quer para? S para sim / N para nao: ').upper()
+        if resposta != 'S':
+            continue
+        else:
             break
-        while True:
-            numero = int(input('Diga um numero: '))
-            qts += 1
-            soma = soma + numero
-            media = soma / qts
-            resposta = input('Quer para? S para sim / N para nao: ').upper()
-            if resposta != 'S':
-                continue
-            else:
-                break
 media = soma / qts
 print(f'A soma desses {qts} numeros são de : {soma}')
-print(f'A media dos {qts} numeros digitados é: {media}')
+print(f'O menor numero é {menor} e o maior numeros é: {maior}')
+print(f'A media dos {qts} numeros digitados é: {media:.2f}')
 print('FIM')
 
