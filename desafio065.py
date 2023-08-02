07a3523f70ed11432b3613292fb5d02d7ae0b028
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-08-02 16:05:38 -0300

    Update desafio065.py

--- desafio065.py
+++ desafio065.py
@@ -1,10 +1,17 @@
+def obter_numero():
+    while True:
+        valor = input('Diga um numero: ').strip()
+        if valor.isnumeric():
+            return int(valor)
+        else:
+            print("Valor inválido. Digite um número válido.")
 
 numero = 0
 qts = 0
 soma = 0
 resposta = ''
 media = 0
-maior= 0
+maior = 0
 menor = 0
 primeira_vez = True
 
@@ -12,24 +19,24 @@ while True:
     if resposta == 'S':
         break
     while True:
-        numero = int(input('Diga um numero: '))
+        numero = obter_numero()
         qts += 1
         soma = soma + numero
         if primeira_vez:
-            menor = numero + 1
+            menor = numero
             primeira_vez = False
         if numero > maior:
             maior = numero
         if menor > numero:
             menor = numero
-        resposta = input('Quer para? S para sim / N para nao: ').upper()
-        if resposta != 'S':
-            continue
+        resposta = input('Quer parar? S para sim / N para nao: ').upper().strip()
+        while resposta != 'S' and resposta != 'N':
+            resposta = input('Quer parar? S para sim / N para nao: ').upper().strip()
         else:
             break
+
 media = soma / qts
-print(f'A soma desses {qts} numeros são de : {soma}')
-print(f'O menor numero é {menor} e o maior numeros é: {maior}')
+print(f'A soma desses {qts} numeros são de: {soma}')
+print(f'O menor numero é {menor} e o maior numero é: {maior}')
 print(f'A media dos {qts} numeros digitados é: {media:.2f}')
 print('FIM')
-
