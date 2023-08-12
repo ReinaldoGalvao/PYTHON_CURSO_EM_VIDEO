0c092c0b5a9b5f260d527304af3fc04073190647
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-08-12 01:31:28 -0300

    Update desafio071.py

--- desafio071.py
+++ desafio071.py
@@ -1,8 +1,10 @@
 
 dinheiro = int(input('Diga quantos quer sacar: '))
+notas200 = dinheiro // 200
+resto200 = dinheiro % 200
 
-notas50 = dinheiro // 50
-resto50 = dinheiro % 50
+notas50 = resto200 // 50
+resto50 = resto200 % 50
 
 notas20 = resto50 // 20
 resto20 = resto50 % 20
@@ -12,6 +14,7 @@ resto10 = resto20 % 10
 
 notas1 = resto10
 
+print(f'{notas200} notas de R$200,00')
 print(f'{notas50} notas de R$50,00')
 print(f'{notas20} notas de R$20,00')
 print(f'{notas10} notas de R$10,00')
