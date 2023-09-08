c1a2998ffb49d1413a5891983aff06a7bcef4e71
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-08 15:31:37 -0300

    refazendo desafio 72

--- desafio072.py
+++ desafio072.py
@@ -3,7 +3,7 @@ Crie um programa que tenha um tupla totalmente preenchida com uma contagem
 por extenso, de zero ate vinte.
 
 Seu programa deverá ler um numero pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
-'''
+
 opcao =''
 numero = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
@@ -20,3 +20,23 @@ while True:
         opcao = input('Quer continuar [S/N]: ').strip().upper()
     if opcao == 'N':
         break
+'''
+
+cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    while True:
+        num = int(input('Digite um numero entre 0 e 20: '))
+        if 0 <= num <=20:
+            break
+        print('Você esta digitando um numero abaixo de 0 ou maior que 20\nTente novamente!')
+    
+    print(f'Voce digitou o numero {cont[num]}')
+    opcao = input('Quer continuar [S/N]: ').strip().upper()
+    if opcao != 'S':
+        break
+
+
+
+
+
--- dicas.py
+++ dicas.py
@@ -1,4 +1,21 @@
 lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
 
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('==============')
+print('==============')
 for cont in range(0, len(lanche)):
-    print(f'Eu vou comer {lanche[cont]}')
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+print('==============')
+print('==============')
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print ('Comi pra caramba!')
+
+print('==============')
+print('==============')
+print(sorted(lanche))
+print(lanche)
+print('==============')
+print('==============')
