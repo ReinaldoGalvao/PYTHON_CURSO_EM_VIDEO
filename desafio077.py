87370c51e80c9d976caad202c02b94ece542ae8d
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-11 16:46:58 -0300

    Create desafio077.py

---
+++ desafio077.py
@@ -0,0 +1,13 @@
+'''
+Crie o programa com varias palavras
+Depois disso, voce deve mostrar, para cada palavra, quais sao as suas vogais
+'''
+
+palavras = ('aprender', 'programar', 'linguagem', 'python',
+            'curso', 'gratis', 'estudar', 'praticar', 'trabalhar',
+            'mercado', 'programador', 'futuro')
+for p in palavras:
+    print(f'\nNa palara temos {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
