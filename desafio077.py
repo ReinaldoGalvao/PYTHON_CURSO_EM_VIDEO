7231d991a7e38ea951ffe31804cc2999ed0bfb5a
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-12 10:01:59 -0300

    Update desafio077.py

--- desafio077.py
+++ desafio077.py
@@ -10,4 +10,4 @@ for p in palavras:
     print(f'\nNa palara temos {p.upper()} temos ', end='')
     for letra in p:
         if letra.lower() in 'aeiou':
-            print(letra, end=' ')
+            print(letra, end=' ') 
