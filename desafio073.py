9e761061841ce3d7565d3ced88e779822f9d5404
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-13 15:59:58 -0300

    Update desafio073.py

--- desafio073.py
+++ desafio073.py
@@ -7,13 +7,17 @@ C) Uma lista com os times em ordem alfabeticas
 D) Em que posição na tabela o Flamengo está
 '''
 
-brasileirao= ('Botafogo', 'Flamengo', 'Fluminense', 'Palmeiras', 'Bragantino', 'Grêmio', 'Athletico-PR', 'Cuiabá', 'São Paulo', 'Atlético-MG', 'Cruzeiro', 'Internacional', 'Fortaleza', 'Corinthians', 'Goiás', 'Bahia', 'Santos', 'Coritiba', 'Vasco', 'America-MG')
+brasileirao= ('Botafogo', 'Palmeiras', 'Fluminense', 'Flamengo', 'Bragantino', 'Grêmio', 'Athletico-PR', 
+              'Cuiabá', 'São Paulo', 'Atlético-MG', 'Cruzeiro', 'Internacional', 'Fortaleza', 
+              'Corinthians', 'Goiás', 'Bahia', 'Santos', 'Coritiba', 'Vasco', 'America-MG')
 
 print(f'Os 5 primeiros times são {brasileirao[:5]}\n1º {brasileirao[0]}\n2º {brasileirao[1]}\n3º {brasileirao[2]}\n4º {brasileirao[3]}\n5º {brasileirao[4]}')
 print('='*10)
 print(f'Os 4 ultimos da tabela são {-4:} \n{brasileirao[16:]}\n17º {brasileirao[16]}\n18º {brasileirao[17]}\n19º {brasileirao[18]}\n20º {brasileirao[19]}')
 print('='*10)
+print('Os times em ordem alfabeticas são: ')
 print(sorted(brasileirao))
+print('='*10)
 print(f'Os times em ordem alfabeticas são: \n{sorted(brasileirao[0:])}')
 print('='*10)
 print(f'O Flamengo esta na {brasileirao.index("Flamengo")+1}ª posição')
