372cef60996e4b1df1b6184a2aa5f5ad71b238df
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-14 11:15:04 -0300

    Update desafio075.py

--- desafio075.py
+++ desafio075.py
@@ -6,18 +6,23 @@ A - Quantas vezes apareceu o valor 9
 B - Em que posição foi digitado o primeiro valor 3
 C - Quais foram os nimeros pares
 '''
-
+cont = 0
 num =  (int(input('Digite um numero: ')),
         int(input('Digite outro numero: ')),
         int(input('Digite mais um numero: ')),
         int(input('Digite o ultimo numero: ')))
-print(f'Voce digotou os numeros {num}')
+print(f'Voce digotou os numeros {" ".join(map(str, num))}')
 print(f'O valor 9 apareceu {num.count(9)} vezes')
+par = []
+impar = []
 if 3 in num:
     print(f'o primeiro numero 3 apareceu na {num.index(3)+1}ª posição')
 else:
     print(f'O valor 3 nao foi digitado em nunhuma posição.')
-print(f'O valores pares digitados foram ', end=' ')
 for n in num:
     if n % 2 == 0:
-        print(n, end=' ')
+        par.append(n)
+    else:
+        impar.append(n)
+print(f'O numeros pares digitados foram {" ".join(map(str, par))}')
+print(f'Todos os numeros digitados foram {" ".join(map(str, num))}')
