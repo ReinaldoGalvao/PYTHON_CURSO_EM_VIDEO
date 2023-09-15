a81cd78c981b1d8007ee3e9bf7832a413c5f8658
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-15 11:03:36 -0300

    Create desafio078.py

---
+++ desafio078.py
@@ -0,0 +1,14 @@
+'''
+Faça um programa que leia 5 valores numericos e guarde em uma lista.
+No final, mostre qual foi o maior e o menor valor digtado e suas respectivas posições na lista.
+'''
+
+valores = []
+for v in range(0, 5):
+    valores.append(int(input(f'Digite um valor: ')))
+    
+    
+print(valores)
+print(f'O maior valor da lista é {max(valores)}')
+print(f'O menor valor da lista é {min(valores)}')
+print(f'O maior valor da lista aparece na posiçaõ {valores.index(max(valores))+1}')
