4bbcda52dbf8f43cba5d6389088e55ad3489ed08
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-15 12:09:04 -0300

    Update desafio078.py

--- desafio078.py
+++ desafio078.py
@@ -10,5 +10,6 @@ for v in range(0, 5):
     
 print(valores)
 print(f'O maior valor da lista é {max(valores)}')
+print(f'O maior valor da lista aparece na posiçaõ {valores.index(max(valores))}')
 print(f'O menor valor da lista é {min(valores)}')
-print(f'O maior valor da lista aparece na posiçaõ {valores.index(max(valores))+1}')
+print(f'O maior valor da lista aparece na posiçaõ {valores.index(min(valores))}')
