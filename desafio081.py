d2ce33649f5610c9b8c9114778c8e4ddc840ad07
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-18 10:03:05 -0300

    Update desafio081.py

--- desafio081.py
+++ desafio081.py
@@ -7,17 +7,35 @@ B) A lista de valores decrescente.
 C) Se o valor 5 foi digitado e esta na lista
 '''
 
+# Inicializa a lista vazia
 lista = []
 
+# Loop para ler os números
 while True:
-    valor = input('Digite um  numero ou [s para sair]: ').lower()
+    valor = input('Digite um número ou [s para sair]: ').lower()
     if valor == 's':
         break
     if valor.isdigit():
-        lista.append(valor)
+        lista.append(int(valor))
     else:
-        print(f'Invalido tente novamente.')
+        print(f'Inválido. Por favor, tente novamente.')
 
+# A) Quantos números foram digitados
+quantidade_numeros = len(lista)
+
+# B) Lista de valores em ordem decrescente
 lista.sort(reverse=True)
-print(lista)
 
+# C) Verifica se o valor 5 está na lista
+valor_5_presente = 5 in lista
+
+# Mostra os resultados
+print(f'Foram digitados {quantidade_numeros} números.')
+print(f'A lista em ordem decrescente é: {lista}.')
+if 5 in lista:
+    posicao_5 = lista.index(5)+1
+    print(f'O numero 5 esta na posiçao {posicao_5}.')
+if valor_5_presente:
+    print(f'O valor 5 foi digitado e está na lista.')
+else:
+    print('O valor 5 não foi digitado ou não está na lista.')
