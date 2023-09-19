ba265fb7967152afa01f8f84e8f90713d57b96c5
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-19 08:08:43 -0300

    Update desafio081.py

--- desafio081.py
+++ desafio081.py
@@ -16,3 +16,7 @@ print('-='*30)
 print(f'Você digitou {len(valores)} numeros.')
 valores.sort(reverse=True)
 print(f"Os valores em orde decrescente são {valores}")
+if 5 in valores:
+    print("O valor 5 faz parte da lista!")
+else:
+    print("O valor 5 não foi encontrado na lista!")
