6422c37d9e94c795c0e4802d3ba06ebb36d7c344
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-22 11:14:28 -0300

    Update desafio090.py

--- desafio090.py
+++ desafio090.py
@@ -7,11 +7,26 @@ No final, mostre o conteúdo da estrutura na tela.
 ano_letivo ={}
 
 ano_letivo['Nome'] = str(input('Nome: '))
-ano_letivo['Média'] = int(input('Diga a média: '))
+ano_letivo['Média'] = float(input(f'Diga a média do aluno {ano_letivo["Nome"]}: '))
 
 print(f"O nome é {ano_letivo['Nome']}")
 print(f"A média é {ano_letivo['Média']}")
-if ano_letivo['Média'] >= 5:
+"""if ano_letivo['Média'] >= 7:
     print(f"O aluno {ano_letivo['Nome']} está aprovado.")
+elif ano_letivo['Média'] >= 5 and ano_letivo['Média'] <= 6.9:
+    print(f"O aluno {ano_letivo['Nome']} está de recuperação.")
 else:
-    print(f"O aluno {ano_letivo['Nome']} está reprovado.")
+    print(f"O aluno {ano_letivo['Nome']} está reprovado.")  """
+    
+if ano_letivo['Média'] >= 7:
+    ano_letivo['Situação'] = 'Aprovado'
+elif 5 <= ano_letivo['Média'] < 7:
+    ano_letivo['Situação'] = 'Recuperação'
+else:
+    ano_letivo['Situação'] =  'Reprovado'
+    
+print('x-'*30)
+
+for k, v in ano_letivo.items():
+    print(f' - {k} é igual a {v}')
+    
