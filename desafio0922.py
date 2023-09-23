fdabbd53065537d53b4edfae4606d1aca30fd0ba
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-23 03:56:45 -0300

    Create desafio0922.py

---
+++ desafio0922.py
@@ -0,0 +1,28 @@
+'''
+Crie um programa que leia:
+Nome, Ano de nascimento, Carteira de trabalho 
+Usando a idade cadastre-os
+Se por acaso a CTPS for diferente a ZERO o dicionário receberá também:
+Ano da contratação e salário
+Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar
+'''
+import datetime
+ano_atual = datetime.date.today().year
+
+#print(ano_atual)
+dados = {}
+
+dados['Nome'] = str(input('Nome: '))
+dados['Ano_nasc'] = int(input('Ano de nascimento [ex 1980]: '))
+dados['CTPS'] = str(input('Carteira de trabalho: '))
+dados['Idade'] = ano_atual - dados["Ano_nasc"]
+if dados['CTPS'] != '':
+    dados['Ano_contrat'] = int(input('Ano de contratação [ex 1995]: '))
+    dados['Salario'] = float(input('Salario: '))
+dados['Anos_aposentar'] = dados['Ano_contrat'] + 35
+print(f'O {dados["Nome"]} tem {dados["Idade"]} anos')
+if dados['Anos_aposentar'] > ano_atual:
+    print(f'O {dados["Nome"]} irá se aposentar em {dados["Anos_aposentar"]} e faltam {dados["Anos_aposentar"] - ano_atual} anos')
+else:
+    print(f'O {dados["Nome"]} já se aposentou em {dados["Anos_aposentar"]}')
+#print(dados)
