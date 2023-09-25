02be70fddcbfa5cd0948ce60dca831e94a245214
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-25 16:48:25 -0300

    Update desafio095.py

---
+++ desafio095.py
@@ -0,0 +1,33 @@
+jogador = {}
+partidas = []
+time = []
+
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Partidas jogadas pelo {jogador["nome"]}: '))
+    partidas.clear()
+    for i in range(1, tot +1):
+        partidas.append(int(input(f'Quantos gols na {i}ª partida: ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N]')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k+1:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
