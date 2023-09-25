9c4cffe7512f676262c07116ee901218b88f6872
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-25 15:49:09 -0300

    Update desafio094.py

--- desafio094.py
+++ desafio094.py
@@ -7,26 +7,43 @@ C) Uma lista com todas as mulheres
 D) Uma lista com todas as pessoas com idade acima da média.
 '''
 
-
-dados = {}
-
+galera = []
+pessoa = {}
+soma = media = 0
 while True:
-    dados['nome'] = input('Nome: ')
-    dados['sexo'] = input('Sexo [M/F]: ').upper()
-    while dados['sexo'] not in ['M', 'F']:
-        dados['sexo'] = input('Incorreto - Sexo [M/F]: ').upper()
-    dados['idade'] = input('Idade: ')
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
     while True:
-        resp = input('Deseja continuar? [S/N]:').upper()
-        if resp == 'S':
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
             break
-        elif resp == 'N':
+        print('INVALIDO! Digite M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N]')).upper()[0]
+        if resp in 'SN':
             break
-        elif resp != 'S' and resp != 'N':
-            continue
-    if resp == 'S':
-        continue
+        print('INVALIDO - Responda S ou N')
     if resp == 'N':
         break
-
-print(dados)
+print('-=' *30)
+print(f'Ao todo temos {len(galera)} pessoas cadastradas')
+print('-=' *30)
+media = soma / len(galera)
+print(f'A média de idade é {media:5.2f} anos')
+print('-=' *30)
+print('As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]} ')
+        
+print('Lista de pessoas com a idade acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('     ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
