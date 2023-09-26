4b6728e5ceb46fe0c08e85372ab909278d510e7e
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-26 14:59:36 -0300

    Update desafio095.py

--- desafio095.py
+++ desafio095.py
@@ -26,8 +26,16 @@ for i in jogador.keys():
 print()
 print('-' * 40)
 for k, v in enumerate(time):
-    print(f'{k+1:>3} ', end='')
+    print(f'{k:>3} ', end='')
     for d in v.values():
         print(f'{str(d):<15}', end='')
     print()
 print('-' * 40)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com código {busca}!')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]}: ')
