bc753ff84bef09c20237f9f06b68903f44823ead
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-26 18:18:27 -0300

    Create desafio096.py

---
+++ desafio096.py
@@ -0,0 +1,15 @@
+'''
+Faça um programa que tenha uma função chamada Area(), que receba as
+dimensões da um terrono retangular (largura e comprimento) e mostre a area do terrono
+'''
+def area (l, c):
+    a = l * c
+    print(f'A area de um terreno {l}x{c} é de {a}')
+
+
+print('Controle de Terrenos')
+print('-' *20)
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+
+area(l, c)
