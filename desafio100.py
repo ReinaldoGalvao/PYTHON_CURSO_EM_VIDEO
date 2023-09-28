123f6de7be0b2ce55329d85c83016f10ac9ce8e0
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-28 09:18:10 -0300

    Update desafio100.py

--- desafio100.py
+++ desafio100.py
@@ -4,17 +4,29 @@ chamadas sorteia() e somaPar().A primeira função vai sortear 5 numeros
 e vai coloca-los dentro da lista e a segunda função vai mostrar a soma 
 entre todos os valores PARES sorteados pela função anterior
 '''
+
+
 from random import randint
 
+
+
+import random
+
+def sorteia():
+    numeros = []
+    for _ in range(5):
+        numeros.append(random.randint(1, 10))
+    return numeros
+
 def somaPar(numeros):
-    if numeros % 2 == 0:
-        print(f'O {numeros} é par.')
+    soma = 0
+    for numero in numeros:
+        if numero % 2 == 0:
+            soma += numero
+    return soma
 
+numeros = sorteia()
+print(f'Números sorteados: {numeros}')
+soma_pares = somaPar(numeros)
+print(f'Soma dos números pares: {soma_pares}')
 
-def sorteia(* numeros):
-    for i in range(1, 5):
-        num = randint(1, 6)
-        somaPar()
-    print(f'Os numeros sorteados foram {num}')
-    
-sorteia()
