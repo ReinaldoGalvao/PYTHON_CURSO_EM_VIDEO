8b8afd3fa29204968e38030dabf8e1d343c89503
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-29 11:02:19 -0300

    Update desafio103.py

--- desafio103.py
+++ desafio103.py
@@ -5,7 +5,7 @@ que receba 2 parametros opcionais: nome de um jogador e quantos gols ele marcou.
 o programa deverá ser capaz de mostrar a FICHA DO JOGADOR, mesmo que algum dado não
 tenha sido informando corretamente.
 '''
-def jogo(jogador, gols):
+'''def jogo(jogador, gols):
     if jogador == '' and gols == '':
         print('O jogador <desconhecido> fez 0 gol(s) no campeonato.')
     elif jogador != '' and gols == '':
@@ -20,3 +20,25 @@ jogador = str(input('Nome do jogador: '))
 gols = str(input('Numeros de gols: '))
 
 jogo(jogador, gols)
+'''
+
+
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
+    
+    
+
+n = str(input('Nome do jogador: '))
+g = str(input('Numeros de gols: '))
+
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+    
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
+    
+
