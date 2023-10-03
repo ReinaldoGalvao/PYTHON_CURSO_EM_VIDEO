d100dce2d3fd29e4b14be5e03ffa8f511177ced0
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-03 17:02:35 -0300

    Update moeda.py

--- moeda.py
+++ moeda.py
@@ -1,23 +1,26 @@
-def aumentar(x, y):
+def aumentar(x=0, y=0):
     soma = x + y
     return soma
 
-def diminuir(x, y):
+def diminuir(x=0, y=0):
     diminuir = x - y
     return diminuir
 
-def dobro(x):
+def dobro(x=0):
     dobro = x * 2
     return dobro
 
-def metade(x):
+def metade(x=0):
     metade = x / 2
     return metade
 
-def porcentagem(x, y):
+def porcentagem(x=0, y=0):
     porcentagem = x + (x * y /100) 
     return porcentagem
 
-def porcentagemmenos(x, y):
+def porcentagemmenos(x=0, y=0):
     porcentagem = x - (x * y /100) 
     return porcentagem
+
+def moeda(x=0, moeda='R$'):
+    return f'{moeda}{x}'.replace('.', ',')
