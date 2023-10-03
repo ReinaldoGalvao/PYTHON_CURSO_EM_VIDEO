cfb3768c3d97da4558f938eadf94c0c9ae746f56
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-03 18:22:31 -0300

    Update moeda.py

--- moeda.py
+++ moeda.py
@@ -1,26 +1,26 @@
-def aumentar(x=0, y=0):
+def aumentar(x=0, y=0, formato=False):
     soma = x + y
-    return soma
+    return soma if formato is False else moeda(soma)
 
-def diminuir(x=0, y=0):
+def diminuir(x=0, y=0, formato=False):
     diminuir = x - y
-    return diminuir
+    return diminuir if formato is False else moeda(diminuir)
 
-def dobro(x=0):
+def dobro(x=0, formato=False):
     dobro = x * 2
-    return dobro
+    return dobro if not formato else moeda(dobro)
 
-def metade(x=0):
+def metade(x=0, formato=False):
     metade = x / 2
-    return metade
+    return metade if not formato else moeda(metade)
 
-def porcentagem(x=0, y=0):
+def porcentagem(x=0, y=0, formato=False):
     porcentagem = x + (x * y /100) 
-    return porcentagem
+    return porcentagem if formato is False else moeda(porcentagem)
 
-def porcentagemmenos(x=0, y=0):
+def porcentagemmenos(x=0, y=0, formato=False):
     porcentagem = x - (x * y /100) 
-    return porcentagem
+    return porcentagem if formato is False else moeda(porcentagem)
 
 def moeda(x=0, moeda='R$'):
     return f'{moeda}{x:.2f}'.replace('.', ',')
