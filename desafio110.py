68c1ad81795d0bf9dc773e3e943a6fa6ee35a5b0
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-06 17:09:10 -0300

    Create desafio110.py

---
+++ desafio110.py
@@ -0,0 +1,10 @@
+'''
+Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada
+resumo(), que mostre na tela algumas informaçoes geradas pelas funçoes que já
+temos no módulo criado até aqui.
+'''
+
+import moeda
+p = int(input('Diga um valor R$: '))
+moeda.resumo(p, 20, 12)
+
