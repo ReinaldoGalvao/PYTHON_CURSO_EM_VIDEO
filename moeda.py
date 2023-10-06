efb4b7d280129d2bf4c75c533e591e87497082c1
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-06 17:09:14 -0300

    Update moeda.py

--- moeda.py
+++ moeda.py
@@ -24,3 +24,14 @@ def porcentagemmenos(x=0, y=0, formato=False):
 
 def moeda(x=0, moeda='R$'):
     return f'{moeda}{x:.2f}'.replace('.', ',')
+
+def resumo(preco =0, taxaa=10, taxar=5):
+    print('-'*40)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*40)
+    print(f'Preço analizado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco ,True)}')
+    print(f'Com {taxaa}% de aumento: \t{porcentagem(preco, taxaa, True)}')
+    print(f'Com {taxar}% de redução: \t{porcentagemmenos(preco, taxar, True)}')
+    print('-'*40)
