c261c2330158029e8d2862d0b684d55e4282d556
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-09 11:25:03 -0300

    ex112

---
+++ ex112/desafio112.py
@@ -0,0 +1,9 @@
+'''
+Crie um pacote chamado utilidadesCeV que tenha dois modulos internos chamados moeda e dado.
+Transfira todas as funções utilizados nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
+'''
+
+from utilidadescev import moeda, dado
+
+p = dado.leiaDinheiro('Diga um valor R$: ')
+moeda.resumo(p, 20, 12)
--- ex112/utilidadescev/dado/__init__.py
+++ ex112/utilidadescev/dado/__init__.py
@@ -1,8 +1,8 @@
 def leiaDinheiro(msg):
     valido = False
     while not valido:
-        entrada = str(input(msg))
-        if entrada.isalpha():
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada == '':
             print(f'ERRO: \"{entrada}\" é um preço invalido')
         else:
             valido = True
