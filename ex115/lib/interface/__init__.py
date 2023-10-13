94f516cd403a1009c6acb2fd19b91ccec07f26d9
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-13 19:21:24 -0300

    Create __init__.py

---
+++ ex115/lib/interface/__init__.py
@@ -0,0 +1,30 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n1 = int(input(msg))
+        except (ValueError, TypeError):
+            print('Você tem que digitar um número válido.')
+            continue
+        except KeyboardInterrupt:
+            print('Entrada de dados enterrompida pelo usuário.')
+            return 0
+        else:
+            return n1
+
+def linha(tam = 42):
+    return '-' * tam
+
+def cabeçalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+    
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c += 1
+    print(linha())
+    opc = leiaInt('\033[32mSua Opção:\033[m ')
+    return opc
