86b8a4955806caa73585cdbe41aed78cc8ff601b
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-13 19:21:29 -0300

    Create sistema.py

---
+++ ex115/sistema.py
@@ -0,0 +1,22 @@
+from lib.interface import *
+from lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        #opção de lsitar o conteudo de um arquivo!
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabeçalho('\033[31mOpção 2\033[m')
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema ..Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
